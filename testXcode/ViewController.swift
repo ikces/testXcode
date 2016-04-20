4c3b56d25a2fbb9fbb9dd3ecfc5f6ec0d827473c
vidsecki <vid.secki@gmail.com>
2016-04-20 21:10:04 +0200

    first commit

---
+++ testXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testXcode
+//
+//  Created by Vid on 20/04/16.
+//  Copyright © 2016 Vid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
